63ee0dfb2ccc38c9bac775bc7bf595ddcba85551
James Cannon <James@thecannonhome.com>
2016-03-10 09:17:36 -0600

    Update DeckProbability.java
    Changed functions from private to public, reformatted, and added comments to everything

--- DeckProbability.java
+++ DeckProbability.java
@@ -3,39 +3,52 @@ package deckprobability;
 /**
  *
  * @author James Cannon
- * @version 7 March 2016 - 9:16 P.M.
+ * @version 10 March 2016 - 9:16 A.M.
  */
-public class DeckProbability 
-{
-    
-    private static double permutations(int n, int r){
-        return factorial(n)/factorial(n-r);
-    }
-    
-    private static double combinations(int n, int r){
-        return permutations(n,r)/factorial(r);
-    }
-    
-    private static double factorial(int n){
-        if (n == 0){
-            return 1;
-        }//if
-        else{
-            return n * factorial(n-1);
-        }
-    }
+public class DeckProbability {
 
     /**
-     * @param args the command line arguments
+     * 
+     * @param n see return
+     * @return n!
      */
-    public static void main(String[] args) 
-    {
+    public static double factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }//if
+        else {
+            return n * factorial(n - 1);
+        }//else
+    }//factorial(int)
+
+    /**
+     * 
+     * @param n see return
+     * @param r see return
+     * @return nPr (n permutations r)
+     */
+    public static double permutations(int n, int r) {
+        return factorial(n) / factorial(n - r);
+    }//permutations(int, int)
+
+    /**
+     * 
+     * @param n see return
+     * @param r see return
+     * @return nCr (n choose r)
+     */
+    public static double combinations(int n, int r) {
+        return permutations(n, r) / factorial(r);
+    }//combinations(int, int)
+
+    public static void main(String[] args) {
         int fullGroup = 10;
         int numOfDesired = 5;
         int selections = 3;
-        System.out.println(combinations(numOfDesired,selections));
-        System.out.println(combinations(fullGroup,selections));
-        System.out.println(combinations(numOfDesired,selections)/combinations(fullGroup,selections));
-    }
+        System.out.println(combinations(numOfDesired, selections));
+        System.out.println(combinations(fullGroup, selections));
+        System.out.println(combinations(numOfDesired, selections)
+                / combinations(fullGroup, selections));
+    }//main(String)
 
-}
+}//DeckProbabilty 
