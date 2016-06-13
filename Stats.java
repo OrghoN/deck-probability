9bb0b008d5272fce23f27a89c0eaa44ecb1aa199
James Cannon <James@thecannonhome.com>
2016-06-13 11:32:11 -0500

    Cleanup
    Comments added

--- Stats.java
+++ Stats.java
@@ -1,65 +1,73 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package stats;
 
 import java.util.ArrayList;
 
 /**
  *
- * @author itworkstudy
+ * @author James Cannon
+ * @version 13 June 2016 - 11:12 A.M.
  */
 public class Stats {
 
-    private static final double SIMULATIONS = 100000000;
-    private static double perfectGame = 0;
-    private static double perfectHand = 0;
-    private static final ArrayList<String> INOPENING = new ArrayList<>();
-    private static final ArrayList<String> FIRSTDRAW = new ArrayList<>();
-    private static final int[] MULLS = new int[6];
+    private static final double SIMULATIONS = 100;
+    private static double perfectGame = 0;//number of perfect games
+    private static double perfectHand = 0;//number of perfect hands
+    private static final ArrayList<String> INOPENING = new ArrayList<>();//List of cards required in opening
+    private static final ArrayList<String> FIRSTDRAW = new ArrayList<>();//List of cards required in first draw
+    private static final int[] MULLS = new int[6];//array for keeping track of mulligans
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        INOPENING.add("Steppe Lynx");
+        INOPENING.add("Steppe Lynx");//init list cards required 
         FIRSTDRAW.add("Atarka's Command");
+        
 
-        for (int i = 0; i < SIMULATIONS; i++) {
-            //System.out.println("\n\nNew Game");
-            Deck.initGame();
-            Deck.shuffle();
-            Deck.draw(7);
-
-            Deck.resolveMulligans();
-
-            MULLS[Deck.scry()]++;
+        for (int i = 0; i < SIMULATIONS; i++) {//loop the simulations
+//            System.out.println("\n\nNew Game");
+            Deck.initGame();//(re)set the game
+            Deck.shuffle();//randomize the deck
+            Deck.draw(7);//move 7 cards from the deck to the hand
+            Deck.resolveMulligans();//resolve mulligans
+            MULLS[Deck.scry()]++;//add stats to MULLS array and scry if necessary
+            /*scry() returns an int equal to 7-hand.size(). Thus, if the simulation
+            required one mulligan, the hand size would be six and scry() returns 1.
+            MULLS[] keeps track of the number of mulls and shows the difference between
+            a mull to 6 and a mull to 3. A keep at 7 goes to MULLS[0].*/
+            
             if (Deck.handCheck(INOPENING) && Deck.hContainsLand(2, 6) && Deck.hContainsFetch(1,6)) {
+                /*The initial if statement calls handCheck, hContainsLand, and hContains Fetch
+                per specifications of the situation. In this case, the hand must contain
+                INOPENING, at least 2 lands, and at least 1 fetch land. If it does, the 
+                variable for recording perfect opening hands, increments, otherwise,
+                the simulations is treated as a failure and a new simulation begins 
+                at the top of the loop.*/
                 perfectHand++;
 //                System.out.println("Perfect Hand");
-                Deck.play();
-                Deck.draw(1);
-
+                Deck.play(0);//play() accounts for fetch lands and other cards that interact with the deck on T1
+                Deck.draw(1);//T2 begins with a draw
                 if (Deck.handCheck(FIRSTDRAW) && Deck.hContainsFetch(2, 6)) {
-                    //System.out.println("Perfect Game");
+                    /*The secondary if statement checks if the hand contains the
+                    required sequence of cards for the second turn. In this case, it 
+                    checks for FIRSTDRAW and 2 fetch lands. If it has the required 
+                    cards, the variable for recording a perfect game is incremented.*/
+//                    System.out.println("Perfect Game");
                     perfectGame++;
                 }
             }
 
         }//for
         int total = 0;
-//        for (int i = 0; i <mulls.length; i++){
-//            total+=mulls[1];
-//        }
+        for (int i = 0; i <MULLS.length; i++){
+            total+=MULLS[i];
+        }
         System.out.println(perfectHand + " perfect hands");
         System.out.println(perfectGame + " perfect games");
-//        System.out.println("total number of arrays stuff: " + total); 
-        System.out.printf("%.3f percent of hands mulled to 6\t",((MULLS[1] - MULLS[2]) / SIMULATIONS * 100) );
-        System.out.printf("%.3f percent of hands mulled to 5\t",((MULLS[2] - MULLS[3]) / SIMULATIONS * 100) );
-        System.out.printf("%.3f percent of hands mulled to 4\n",((MULLS[3] - MULLS[4]) / SIMULATIONS * 100) );
-        System.out.printf("%.3f percent of hands mulled to 3\t",((MULLS[4] - MULLS[5]) / SIMULATIONS * 100) );
+        System.out.printf("%.3f percent of hands mulled to 6\t",((MULLS[1]) / SIMULATIONS * 100) );
+        System.out.printf("%.3f percent of hands mulled to 5\t",((MULLS[2]) / SIMULATIONS * 100) );
+        System.out.printf("%.3f percent of hands mulled to 4\n",((MULLS[3]) / SIMULATIONS * 100) );
+        System.out.printf("%.3f percent of hands mulled to 3\t",((MULLS[4]) / SIMULATIONS * 100) );
         System.out.printf("%.3f percent of hands mulled to 2 or less\n",(MULLS[5] / SIMULATIONS * 100) );
         System.out.printf("%.3f percent of hands were perfect\n", (perfectHand / SIMULATIONS * 100));
         System.out.printf("%.3f percent of games were perfect\n", (perfectGame/SIMULATIONS*100));
